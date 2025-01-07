e1af050ab0fdfa32307c13a5da00159111d826e0
Krunoslav Husak <kruno@husak.me>
2025-01-07 22:18:33 +0100

    Flowbite svelte reconfigured in TS version

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { Alert, Button } from "flowbite-svelte";
+  import { InfoCircleSolid } from "flowbite-svelte-icons";
+</script>
+
+<div class="p-8">
+  <Alert color="green" dismissable>
+    <InfoCircleSolid slot="icon" class="w-5 h-5" />
+    A simple info alert with an
+    <a
+      href="/"
+      class="font-semibold underline hover:text-green-800 dark:hover:text-green-900"
+      >example link</a
+    >
+    . Give it a click if you like.
+  </Alert>
+</div>
