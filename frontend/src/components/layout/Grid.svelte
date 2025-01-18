fe7c4911b2b28375cd037bc38b89b7594afc7809
Krunoslav Husak <kruno@husak.me>
2025-01-18 21:06:01 +0100

    Updated Card and Grid to use snippet instead of slot

--- frontend/src/components/layout/Grid.svelte
+++ frontend/src/components/layout/Grid.svelte
@@ -1,5 +1,16 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+
+  interface Grid {
+		cols: number;
+    children: Snippet;
+	}
+
+	let { cols, children }: Grid = $props();
+</script>
+
 <div class="container mx-auto p-4">
-  <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-    <slot />
+  <div class="grid grid-cols-1 md:grid-cols-{cols} gap-4">
+    {@render children()}
   </div>
 </div>
