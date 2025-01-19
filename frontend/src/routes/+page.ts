541aa0de5bff95bf3883689220b5508f6c06564e
Krunoslav Husak <kruno@husak.me>
2025-01-19 13:04:51 +0100

    SImplified page loading

--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -1,17 +1,9 @@
 import type { PageLoad } from './$types';
-import type { WeatherData } from '$lib/types/weather';
 import { fetchForecast } from '$svc/forecast';
 
-interface PageData {
-  forecast: WeatherData;
-  streamed?: {};
-}
-
-export const load = (async ({ fetch, params }): Promise<PageData> => {
-  const forecast: WeatherData = await fetchForecast(fetch);
-  
-  return {
-    forecast: forecast,
-    streamed: {},
+export const load: PageLoad = async ({ fetch }) => {
+  const forecast = await fetchForecast(fetch);
+  return { 
+    forecast 
   };
-}) satisfies PageLoad;
+};
