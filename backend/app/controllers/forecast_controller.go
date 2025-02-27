6a78ed70e1262d650240bb231c9805413c3234e5
Krunoslav Husak <kruno@husak.me>
2025-02-27 13:42:43 +0100

    Updated backend

--- backend/app/controllers/forecast_controller.go
+++ backend/app/controllers/forecast_controller.go
@@ -3,8 +3,8 @@ package controllers
 import (
 	"strconv"
 
+	"github.com/go-raptor/errs"
 	"github.com/go-raptor/raptor/v3"
-	"github.com/go-raptor/raptor/v3/core"
 	"github.com/h00s/weather/app/services"
 )
 
@@ -19,23 +19,23 @@ func (fc *ForecastController) Get(c *raptor.Context) error {
 	lon := c.QueryParam("lon")
 
 	if lat == "" || lon == "" {
-		return core.NewErrorBadRequest("lat and lon query parameters are required")
+		return c.JSONError(errs.NewErrorBadRequest("lat and lon query parameters are required"))
 	}
 
 	latitude, err := strconv.ParseFloat(lat, 64)
 	if err != nil {
-		return core.NewErrorBadRequest("invalid lat parameter")
+		return c.JSONError(errs.NewErrorBadRequest("invalid lat parameter"))
 	}
 
 	longitude, err := strconv.ParseFloat(lon, 64)
 	if err != nil {
-		return core.NewErrorBadRequest("invalid lon parameter")
+		return c.JSONError(errs.NewErrorBadRequest("invalid lon parameter"))
 	}
 
 	forecast, err := fc.Forecast.GetForecast(latitude, longitude)
 	if err != nil {
-		return core.NewErrorBadRequest(err.Error())
+		return c.JSONError(errs.NewErrorBadRequest(err.Error()))
 	}
 
-	return c.JSON(forecast)
+	return c.JSONResponse(forecast)
 }
--- backend/go.mod
+++ backend/go.mod
@@ -3,20 +3,23 @@ module github.com/h00s/weather
 go 1.24.0
 
 require (
-	github.com/go-raptor/raptor/v3 v3.3.5
+	github.com/go-raptor/errs v1.0.0
+	github.com/go-raptor/raptor/v3 v3.4.4
 	github.com/h00s/goopenmeteo v1.0.1
 )
 
 require (
-	github.com/go-raptor/connector v1.0.0 // indirect
+	github.com/go-raptor/components v1.0.2 // indirect
+	github.com/go-raptor/config v1.0.0 // indirect
+	github.com/go-raptor/connector v1.0.1 // indirect
 	github.com/labstack/echo/v4 v4.13.3 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/pwntr/tinter v1.1.2 // indirect
+	github.com/pwntr/tinter v1.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.33.0 // indirect
+	golang.org/x/crypto v0.35.0 // indirect
 	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
