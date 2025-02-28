6273d7547a30ea7f01515c37a5facf4310811bb6
Krunoslav Husak <kruno@husak.me>
2025-02-28 13:11:23 +0100

    Add theme CSS

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,8 +1,1 @@
 <h1 class="text-3xl font-bold underline">Hello world!</h1>
-
-<style lang="postcss">
-  @reference "tailwindcss/theme";
-  :global(html) {
-    background-color: theme(--color-gray-100);
-  }
-</style>
