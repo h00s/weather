fa9ba84ed455e6a1bbcd7cab3bcd198db0eae2dd
Krunoslav Husak <kruno@husak.me>
2025-03-02 08:21:50 +0100

    Add weather icon

--- frontend/src/lib/components/app/WeatherCard.svelte
+++ frontend/src/lib/components/app/WeatherCard.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+  import WeatherIcon from "$comp/app/WeatherIcon.svelte";
   import type { WeatherCard } from "$type/WeatherCard";
   import { formatTime } from "$util/datetime";
 
   const { weather } = $props<{ weather: WeatherCard }>();
+
+  console.log(weather);
 
   const formattedTime = $derived(formatTime(weather.timestamp));
 </script>
@@ -16,21 +19,7 @@
 
   <!-- Middle section - Weather icon -->
   <div class="flex justify-center">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="48"
-      height="48"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="lucide-icon lucide lucide-cloudy text-blue-100"
-    >
-      <path d="M17.5 21H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z"></path>
-      <path d="M22 10a3 3 0 0 0-3-3h-2.207a5.502 5.502 0 0 0-10.702.5"></path>
-    </svg>
+    <WeatherIcon code={weather.code} />
   </div>
 
   <!-- Right section - Temperature and conditions -->
