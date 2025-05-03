a5923b1393348f0853a1b59076f67150f25a126c
Krunoslav Husak <kruno@husak.me>
2025-05-04 00:30:00 +0200

    Add SPA controller

--- backend/config/components/controllers.go
+++ backend/config/components/controllers.go
@@ -1,6 +1,7 @@
 package components
 
 import (
+	"github.com/go-raptor/controllers/spa"
 	"github.com/go-raptor/raptor/v4"
 	"github.com/h00s/weather/app/controllers"
 )
@@ -8,5 +9,6 @@ import (
 func Controllers() raptor.Controllers {
 	return raptor.Controllers{
 		&controllers.ForecastController{},
+		spa.NewSPAController("public", "index.html"),
 	}
 }
--- backend/go.mod
+++ backend/go.mod
@@ -4,6 +4,7 @@ go 1.24.2
 
 require (
 	github.com/go-logistiq/logistiq v1.0.14
+	github.com/go-raptor/controllers/spa v1.0.3
 	github.com/go-raptor/middlewares/cors v1.0.1
 	github.com/go-raptor/raptor/v4 v4.0.0-alpha.5
 	github.com/h00s/goopenmeteo v1.0.1
